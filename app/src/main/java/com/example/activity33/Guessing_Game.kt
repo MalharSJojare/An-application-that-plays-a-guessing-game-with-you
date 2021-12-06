7832bc24b8d572f2b218003eb18cd0aad43502c1
unknown <Administrator@KANINI-LTP-465.gisbizdev.gisbiz.chennai.com>
2021-12-06 16:17:19 +0530

    Third Screen Included

--- app/src/main/java/com/example/activity33/Guessing_Game.kt
+++ app/src/main/java/com/example/activity33/Guessing_Game.kt
@@ -12,6 +12,7 @@ import com.google.android.material.textfield.TextInputLayout
 class Guessing_Game : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val randomNum=rand(0,1000)
+        var counter=12
         Log.d("Screen2","Random Number Value=$randomNum")
 
         super.onCreate(savedInstanceState)
@@ -20,16 +21,30 @@ class Guessing_Game : AppCompatActivity() {
         val GuessBtn=findViewById<TextView>(R.id.TextView_PushBtn)
         val PredictText=findViewById<TextView>(R.id.TextView_Prediction)
         val ResetBtn=findViewById<TextView>(R.id.TextView_Reset)
+        val newScreenIntent=Intent(this,ResultPage::class.java)
+        newScreenIntent.putExtra("Value1","$randomNum")
         Log.d("Screen2","Value of Input num=$InputNum")
 //        val num=InputNum.editText.toString().toInt() TextInputEditText
         PredictText.setVisibility(View.GONE)
         GuessBtn.setOnClickListener{
+            counter--
             val num1= InputNum.editText?.text.toString()
             val num=num1.toInt()
             Log.d("Screen2","Input Number Value=$num")
             PredictText.setVisibility(View.VISIBLE)
             PredictText.text= validaterand(num,randomNum)
-            Log.d("Screen2","Result=$randomNum")
+            if(PredictText.text=="Congratulations, you have guessed correct number")
+            {
+                Log.d("Screen2","Moving to next screen")
+                newScreenIntent.putExtra("Result","You Won! The Number is")
+                startActivity(newScreenIntent)
+            }
+            else if(counter==0)
+            {
+                Log.d("Screen2","Moving to next screen")
+                newScreenIntent.putExtra("Result","You lost after 12 attempts. The number is")
+                startActivity(newScreenIntent)
+            }
         }
         ResetBtn.setOnClickListener()
         {
